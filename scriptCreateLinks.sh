383b6d092ab6ec9c3a8ad9d1ed46e5c6ceca51fe
Julien Rabatel <jrabatel@gmail.com>
2012-03-03 22:07:51 +0100

    Forgot to add the script file in previous commit

---
+++ scriptCreateLinks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~
+
+# create the link for the .vimrc file
+rm .vimrc
+ln -s .my-config/vim_config/.vimrc .vimrc
+
+# create the link for the .vim folder
+rm -R .vim
+ln -s .my-config/vim_config/.vim .vim
+
+# create the link for the vimwiki folder
+rm -R vimwiki
+ln -s .my-config/vim_config/vimwiki vimwiki
