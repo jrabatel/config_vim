624e7a727e38ae6a13acef74bd0d2e32f667fec8
Julien Rabatel <Julien.Rabatel@cs.kuleuven.be>
2012-04-02 18:28:14 +0200

    Modification of the link creation script

--- scriptCreateLinks.sh
+++ scriptCreateLinks.sh
@@ -4,12 +4,12 @@ cd ~
 
 # create the link for the .vimrc file
 rm .vimrc
-ln -s .my-config/vim_config/.vimrc .vimrc
+ln -s ~/.my-config/vim_config/.vimrc .vimrc
 
 # create the link for the .vim folder
 rm -R .vim
-ln -s .my-config/vim_config/.vim .vim
+ln -s ~/.my-config/vim_config/.vim .vim
 
 # create the link for the vimwiki folder
 rm -R vimwiki
-ln -s .my-config/vim_config/vimwiki vimwiki
+ln -s ~/.my-config/vim_config/vimwiki vimwiki
